5efb554a0cce21252bd4c5bfe53e6d061820385f
Tomer Barletz <barletz@gmail.com>
2013-08-04 08:40:33 -0700

    Gracefully exit the program

--- test/simple.c
+++ test/simple.c
@@ -4,10 +4,13 @@ int main(void)
 {
     sqlite3MallocInit();
 
-    void *p = sqlite3Malloc(100);
+    void *p = sqlite3Malloc(256);
     if (!p)
     {
         printf("Malloc failed\n");
+
+        sqlite3MallocEnd();
+
         return -1;
     }
 
